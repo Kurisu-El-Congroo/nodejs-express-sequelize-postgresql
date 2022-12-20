6de8b4639681ac3f3a3fda679d9ed181eaee6ae4
Gogs <gogs@fake.local>
2022-12-20 14:10:06 +0800

    A1:Rev-0(mod):C-9 *add sync method for model

--- server.js
+++ server.js
@@ -16,6 +16,15 @@ app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
+// add sync for model
+const db = require("./app/models");
+db.sequelize.sync()
+  .then(() => {
+    console.log("Synced db.");
+  })
+  .catch((err) => {
+    console.log("Failed to sync db: " + err.message);
+  });
 // simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to bezkoder application." });
